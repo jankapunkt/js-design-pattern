e019061a1ff79e22843a866af861cd932c4c29b2
jankapunkt <jkuester@uni-bremen.de>
2017-09-11 13:05:38 +0200

    implemented/tested creational/DependencyInjection

---
+++ tests/creational/DependencyInjection.tests.js
@@ -0,0 +1,45 @@
+import {chai, assert} from 'chai';
+
+import {ServiceWithConstructorInjection, ServiceWithSetterInjection} from '../../src/creational/DependencyInjection';
+
+describe("creational/DependencyInjection", function () {
+
+	it("constructor injection", function () {
+
+		const context = "http://domain.tld";
+
+		const serviceInstance = new ServiceWithConstructorInjection({
+			getContext: function () {
+				return context;
+			},
+		});
+
+		assert.equal(serviceInstance.execute(), context);
+	});
+
+	it("setter injection", function () {
+		const setterServiceInstance = new ServiceWithSetterInjection();
+		const context = "http://domain.tld";
+
+		assert.throws(function () {
+			setterServiceInstance.execute();
+		});
+
+		setterServiceInstance.setContextProvider({
+			getContext(){
+				return context;
+			}
+		})
+		assert.equal(setterServiceInstance.execute(), context);
+
+		const otherContext = "http://subdomain.domain.tld";
+		setterServiceInstance.setContextProvider({
+			getContext(){
+				return otherContext;
+			}
+		})
+		assert.equal(setterServiceInstance.execute(), otherContext);
+		assert.notEqual(setterServiceInstance.execute(), context);
+	});
+
+});
